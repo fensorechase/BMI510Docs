4c53f5bfd863b4005234965d4bee08765ee0a313
Chase M Fensore <cmf2018@gmail.com>
2023-05-01 15:29:22 -0400

    First

---
+++ bmi510-template.R
@@ -0,0 +1,42 @@
+# bmi510.R
+
+#The rando function should return a tibble containing the sampled rows, not the indices of the sampled rows.
+rando = function(x,n=1,replace=T) 0
+
+is_min = function(x,na.rm=T) 0
+
+is_max = function(x,na.rm=T) 0
+
+rep_mat = function(x, M=1, N=1) 0
+
+classes = function(x) 0
+
+df_scale = function(x, center = T, scale = T) 0
+
+log_likelihood_norm = function(x, mean, sd) 0
+
+log_likelihood_unif = function(x, min, max) 0
+
+log_likelihood_chisq = function(x, df) 0
+
+log_likelihood_f = function(x, df1, df2) 0
+
+log_likelihood_t = function(x, df) 0
+
+sensitivity = function(pred,truth) 0
+
+specificity = function(pred,truth) 0
+
+precision = function(pred,truth) 0
+
+recall = function(pred,truth) 0
+
+accuracy = function(pred,truth) 0
+
+f1 = function(pred,truth) 0
+
+minimum_n_per_group = function(d,power = 0.8) 0
+
+r2 = function(pred,truth) 0
+
+adj_R2 = function(pred,truth,n_p) 0
